4c4d5127ef0efa846fb96a35f4605f73a76ea4fb
syehan269 <syehan269@gmail.com>
2020-05-02 20:21:17 +0700

    send extra

--- app/src/main/java/com/syehan/smk_coding_c1/Activity_Detail.kt
+++ app/src/main/java/com/syehan/smk_coding_c1/Activity_Detail.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity__detail.*
+import kotlinx.android.synthetic.main.activity_home.*
 
 class Activity_Detail : AppCompatActivity() {
 
@@ -16,5 +18,26 @@ class Activity_Detail : AppCompatActivity() {
             val intent = Intent(this, Activity_About::class.java)
             startActivity(intent)
         })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setValue()
+    }
+
+    private fun setValue() {
+        val name: String = intent.getStringExtra("getName")
+        val address: String = intent.getStringExtra("getAddress")
+        val gender: String = intent.getStringExtra("getGender")
+        val age: String = intent.getStringExtra("getAge")
+        val email: String = intent.getStringExtra("getEmail")
+        val tele: String = intent.getStringExtra("getTele")
+
+        tv_name.text = ": $name"
+        tv_address.text = ": $address"
+        tv_age.text = ": $age"
+        tv_email.text = ": $email"
+        tv_tele.text = ": $tele"
+        tv_gender.text = ": $gender"
     }
 }
--- app/src/main/java/com/syehan/smk_coding_c1/activity_home.kt
+++ app/src/main/java/com/syehan/smk_coding_c1/activity_home.kt
@@ -3,6 +3,9 @@ package com.syehan.smk_coding_c1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -12,10 +15,37 @@ class activity_home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        btn_submit.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, Activity_Detail::class.java)
-            startActivity(intent)
-        })
 
+
+        btn_submit.setOnClickListener(View.OnClickListener {
+
+            val getName: String? = et_name.text.toString()
+            val getAddress: String? = et_address.text.toString()
+            val getGender: String? = spn_gender.selectedItem.toString()
+            val getAge: String? = et_age.text.toString()
+            val getEmail: String? = et_email.text.toString()
+            val getTele: String? = et_tele.text.toString()
+
+            val intent = Intent(this, Activity_Detail::class.java)
+            intent.putExtra("getName", getName)
+            intent.putExtra("getAddress", getAddress)
+            intent.putExtra("getGender", getGender)
+            intent.putExtra("getAge", getAge)
+            intent.putExtra("getEmail", getEmail)
+            intent.putExtra("getTele", getTele)
+            startActivity(intent)
+
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
